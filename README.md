489faecbd6fee1edbafb77fce339067702b8b153
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2023-06-08 14:34:20 +0200

    Next app (#1)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # flex-internal-frontend
+
+Innlogging er begrenset til AD gruppen team flex.
+
+Adressen til appen er https://flex-internal.intern.nav.no/ i prod og https://flex-internal.intern.dev.nav.no/ i dev. Man må ha naisdevice for å nå appen.
+
+## Henvendelser
+
+Spørsmål knyttet til koden eller prosjektet kan stilles til `flex@nav.no`
+
+## For NAV-ansatte
+
+Interne henvendelser kan sendes via Slack i kanalen `#flex`.
---
+++ src/pages/_app.tsx
@@ -0,0 +1,48 @@
+import '../style/global.css'
+
+import type { AppProps } from 'next/app'
+import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { Header } from '@navikt/ds-react-internal'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Modal } from '@navikt/ds-react'
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        /* Setting this to true causes the request to be immediately executed after initial
+                           mount Even if the query had data hydrated from the server side render */
+                        refetchOnMount: false,
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            }),
+    )
+
+    useEffect(() => {
+        Modal.setAppElement('#__next')
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <title>Flex internal frontend</title>
+                <meta name="robots" content="noindex" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Header>
+                    <Header.Title as="h1">Flex internal frontend</Header.Title>
+                </Header>
+                <div id="root" className="mx-auto p-4 pb-32">
+                    <Component {...pageProps} />
+                </div>
+            </QueryClientProvider>
+        </>
+    )
+}
+
+export default MyApp
