a1ba43ec32211b773a54c491128c286d2248fb40
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-06-12 10:17:29 +0200

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { BodyShort, TextField } from '@navikt/ds-react'
+import { Timeline } from '@navikt/ds-react-internal'
+import { PersonIcon } from '@navikt/ds-icons'
 
 import { initialProps } from '../initialprops/initialProps'
 import { useSoknader } from '../queryhooks/useSoknader'
@@ -22,9 +24,38 @@ const Index = (): JSX.Element => {
                 {soknader?.map((s, idx) => (
                     <BodyShort as="li" key={idx}>
                         {s.id}
+                        {JSON.stringify(s)}
                     </BodyShort>
                 ))}
             </BodyShort>
+                
+
+            { soknader && soknader?.length > 0 && 
+            <Timeline   >
+                <Timeline.Row label="Person" icon={<PersonIcon aria-hidden />}>
+                    {soknader?.map((s, idx) => (
+                        <Timeline.Period key={idx} start={s.fom} end={s.tom} status="success">
+                            {s ?? null}
+
+                        </Timeline.Period>
+                    ))}
+                </Timeline.Row>
+            </Timeline>
+}
+
+            {/* <div className="min-w-[800px] overflow-x-auto">
+                <Timeline>
+                    <Timeline.Row label="Person" icon={<PersonIcon aria-hidden />}>
+                        {soknader?.map((s: any, i) => {
+                            return (
+                                <Timeline.Period key={i} start={s.fom} end={s.tom} status="success">
+                                    child
+                                </Timeline.Period>
+                            )
+                        })}
+                    </Timeline.Row>
+                </Timeline>
+            </div> */}
         </>
     )
 }
