84f4b8801cf3b7d2a22bd9c52dc65ce38fddcf78
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-06-12 10:58:28 +0200

    Flytter filter ut i egen komponent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react'
-import { Chips, TextField } from '@navikt/ds-react'
+import { TextField } from '@navikt/ds-react'
 import { Timeline } from '@navikt/ds-react-internal'
 
 import { initialProps } from '../initialprops/initialProps'
 import { Soknad, useSoknader } from '../queryhooks/useSoknader'
-
-interface Filter {
-    prop: string
-    verdi: string
-    inkluder: boolean
-}
+import { Filter, FilterFelt, ValgteFilter } from '../components/Filter'
 
 const Index = () => {
     const [fnr, setFnr] = useState<string>()
@@ -28,70 +23,6 @@ const Index = () => {
         setFnr(fnrChange.length == 11 ? fnrChange : undefined)
     }
 
-    function ValgteFilter() {
-        if (filter.length === 0) {
-            return <></>
-        }
-
-        return (
-            <Chips>
-                {filter.map((f) => (
-                    <Chips.Removable
-                        key={f.prop}
-                        variant="action"
-                        onClick={() => setFilter((prev) => prev.filter((f2) => f2 !== f))}
-                    >
-                        {f.prop + (f.inkluder ? ' = ' : ' != ') + f.verdi}
-                    </Chips.Removable>
-                ))}
-            </Chips>
-        )
-    }
-
-    function FilterFelt({ prop, verdi }: { prop: string; verdi: string }) {
-        const inkluder: Filter = {
-            prop: prop,
-            verdi: verdi,
-            inkluder: true,
-        }
-        const ekskluder: Filter = {
-            prop: prop,
-            verdi: verdi,
-            inkluder: false,
-        }
-
-        function finnes(array: Filter[], b: Filter) {
-            return array.some((a) => a.prop === b.prop && a.verdi === b.verdi && a.inkluder === b.inkluder)
-        }
-
-        return (
-            <Chips className="inline-flex">
-                <Chips.Toggle
-                    selected={finnes(filter, inkluder)}
-                    onClick={() => {
-                        setFilter((prev) =>
-                            finnes(prev, inkluder) ? prev.filter((x) => !finnes([x], inkluder)) : [...prev, inkluder],
-                        )
-                    }}
-                >
-                    +
-                </Chips.Toggle>
-                <Chips.Toggle
-                    selected={finnes(filter, ekskluder)}
-                    onClick={() => {
-                        setFilter((prev) =>
-                            finnes(prev, ekskluder)
-                                ? prev.filter((x) => !finnes([x], ekskluder))
-                                : [...prev, ekskluder],
-                        )
-                    }}
-                >
-                    -
-                </Chips.Toggle>
-            </Chips>
-        )
-    }
-
     function Tidslinje() {
         if (soknader === undefined || filtrerteSoknader.length === 0) {
             return <></>
@@ -105,14 +36,26 @@ const Index = () => {
                             <Timeline.Period start={s.fom} end={s.tom} status="success">
                                 <ul>
                                     <li>
-                                        sok id: {s.id} <FilterFelt prop="id" verdi={s.id} />
+                                        sok id: {s.id}{' '}
+                                        <FilterFelt prop="id" verdi={s.id} filter={filter} setFilter={setFilter} />
                                     </li>
                                     <li>
                                         syk id: {s.sykmeldingId}{' '}
-                                        <FilterFelt prop="sykmeldingId" verdi={s.sykmeldingId} />
+                                        <FilterFelt
+                                            prop="sykmeldingId"
+                                            verdi={s.sykmeldingId}
+                                            filter={filter}
+                                            setFilter={setFilter}
+                                        />
                                     </li>
                                     <li>
-                                        status: {s.status} <FilterFelt prop="status" verdi={s.status} />
+                                        status: {s.status}{' '}
+                                        <FilterFelt
+                                            prop="status"
+                                            verdi={s.status}
+                                            filter={filter}
+                                            setFilter={setFilter}
+                                        />
                                     </li>
                                 </ul>
                             </Timeline.Period>
@@ -127,7 +70,7 @@ const Index = () => {
         <div className="flex-row space-y-4">
             <TextField type="number" label="fnr" onChange={(e) => fnrInput(e.target.value)} />
 
-            <ValgteFilter />
+            <ValgteFilter filter={filter} setFilter={setFilter} />
 
             <Tidslinje />
         </div>
