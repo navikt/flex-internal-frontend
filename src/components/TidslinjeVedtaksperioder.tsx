be4f0abe08aa99a5251c0299112132ffdffb6259
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-18 10:51:54 +0200

    Pins på varselet

--- src/components/TidslinjeVedtaksperioder.tsx
+++ src/components/TidslinjeVedtaksperioder.tsx
@@ -1,45 +1,71 @@
-import { BodyShort, Heading, Table, Timeline } from '@navikt/ds-react'
+import { BodyShort, Table, Timeline } from '@navikt/ds-react'
 import React, { Fragment } from 'react'
 import dayjs from 'dayjs'
 
-import { dayjsToDate } from '../queryhooks/useSoknader'
-import { VedtaksperiodeResponse } from '../queryhooks/useVedtaksperioder'
+import { StatusHistorikk, StatusHistorikk2, VedtaksperiodeResponse } from '../queryhooks/useVedtaksperioder'
 
 export function TidslinjeVedtaksperioder({ vedtaksperioder }: { vedtaksperioder: VedtaksperiodeResponse[] }) {
+    const varslerSendt = [] as StatusHistorikk2[]
+    const varselEvents = [
+        'DITT_SYKEFRAVAER_MANGLER_INNTEKTSMELDING_SENDT',
+        'DITT_SYKEFRAVAER_MOTTATT_INNTEKTSMELDING_SENDT',
+    ]
+    const datoer = [] as dayjs.Dayjs[]
+    vedtaksperioder.forEach((vp) => {
+        vp.statusHistorikk.forEach((sh) => {
+            datoer.push(dayjs(vp.statusHistorikk[0].vedtakFom))
+            datoer.push(dayjs(vp.statusHistorikk[0].vedtakTom))
+            sh.statusHistorikk.forEach((status) => {
+                datoer.push(dayjs(status.opprettet))
+                if (varselEvents.includes(status.status)) {
+                    varslerSendt.push(status)
+                }
+            })
+        })
+    })
+
+    const eldsteDato = datoer.sort((a, b) => (dayjs(a).isBefore(dayjs(b)) ? -1 : 1))[0]
+    const nyesteDato = datoer.sort((a, b) => (dayjs(a).isBefore(dayjs(b)) ? 1 : -1))[0]
+
     return (
         <div className="min-w-[800px] min-h-[2000px] overflow-x-auto">
-            <Timeline>
+            <Timeline endDate={nyesteDato.add(1, 'week').toDate()} startDate={eldsteDato.toDate()}>
                 {vedtaksperioder.map((vp) => {
                     return (
                         <Timeline.Row key={vp.orgnr} label={vp.orgnr}>
                             {vp.statusHistorikk.map((sh) => {
-                                const sisteStatus = sh.statusHistorikk[sh.statusHistorikk.length - 1]
-
+                                const vedtaksperiodeLesbar = `${dayjs(sh.vedtakFom).format(' D MMMM')} til ${dayjs(sh.vedtakTom).format(' D MMMM')}`
                                 return (
                                     <Timeline.Period
-                                        start={dayjsToDate(sh.vedtakFom)!}
-                                        end={dayjsToDate(sh.vedtakTom)!}
+                                        start={dayjs(sh.vedtakFom).toDate()}
+                                        end={dayjs(sh.vedtakTom).toDate()}
                                         status="neutral"
                                         key={sh.id}
+                                        icon={<span>{sisteVedtaksperiodestatus(sh)}</span>}
                                     >
                                         <Fragment>
-                                            <Heading size="small">{sisteStatus.status}</Heading>
-                                            <BodyShort>{sh.orgNavn}</BodyShort>
-                                            <BodyShort>{sh.vedtakFom}</BodyShort>
-                                            <BodyShort>{sh.vedtakTom}</BodyShort>
-                                            <BodyShort>Statushistorikk</BodyShort>
+                                            <BodyShort className="font-bold" spacing>
+                                                {vedtaksperiodeLesbar}
+                                            </BodyShort>
+                                            <BodyShort spacing>Statushistorikk</BodyShort>
                                             <Table size="small">
                                                 <Table.Body>
-                                                    {sh.statusHistorikk.map((status) => {
-                                                        return (
-                                                            <Table.Row key={status.id}>
-                                                                <Table.DataCell>
-                                                                    {dayjs(status.opprettet).format()}
-                                                                </Table.DataCell>
-                                                                <Table.DataCell>{status.status}</Table.DataCell>
-                                                            </Table.Row>
+                                                    {sh.statusHistorikk
+                                                        .sort((a: StatusHistorikk2, b: StatusHistorikk2) =>
+                                                            dayjs(a.opprettet).isBefore(dayjs(b.opprettet)) ? 1 : -1,
                                                         )
-                                                    })}
+                                                        .map((status) => {
+                                                            return (
+                                                                <Table.Row key={status.id}>
+                                                                    <Table.DataCell>
+                                                                        {dayjs(status.opprettet).format(
+                                                                            'D MMM YYYY HH:mm',
+                                                                        )}
+                                                                    </Table.DataCell>
+                                                                    <Table.DataCell>{status.status}</Table.DataCell>
+                                                                </Table.Row>
+                                                            )
+                                                        })}
                                                 </Table.Body>
                                             </Table>
                                         </Fragment>
@@ -50,13 +76,28 @@ export function TidslinjeVedtaksperioder({ vedtaksperioder }: { vedtaksperioder:
                     )
                 })}
 
-                <Timeline.Zoom>
-                    <Timeline.Zoom.Button label="3 mnd" interval="month" count={3} />
-                    <Timeline.Zoom.Button label="6 mnd" interval="month" count={6} />
-                    <Timeline.Zoom.Button label="12 mnd" interval="month" count={12} />
-                    <Timeline.Zoom.Button label="18 mnd" interval="month" count={18} />
-                </Timeline.Zoom>
+                {varslerSendt.map((vp, i) => {
+                    return (
+                        <Timeline.Pin key={i} date={dayjs(vp.opprettet).toDate()}>
+                            <p>{vp.status}</p>
+                        </Timeline.Pin>
+                    )
+                })}
             </Timeline>
         </div>
     )
+}
+
+const vedtaksperiodeStatuser = [
+    'MANGLER_INNTEKTSMELDING',
+    'HAR_INNTEKTSMELDING',
+    'TRENGER_IKKE_INNTEKTSMELDING',
+    'BEHANDLES_UTENFOR_SPLEIS',
+    'VEDTAK_FATTET',
+]
+
+function sisteVedtaksperiodestatus(statusHistorikk: StatusHistorikk) {
+    return statusHistorikk.statusHistorikk
+        .sort((a: StatusHistorikk2, b: StatusHistorikk2) => (dayjs(a.opprettet).isBefore(dayjs(b.opprettet)) ? 1 : -1))
+        .find((sh) => vedtaksperiodeStatuser.includes(sh.status))?.status
 }
