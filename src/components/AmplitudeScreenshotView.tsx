6ab5c68b80bde6d94dbf7344d6c75d32b86f448e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-24 11:22:58 +0200

    screenshots

--- src/components/AmplitudeScreenshotView.tsx
+++ src/components/AmplitudeScreenshotView.tsx
@@ -10,6 +10,7 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
     const [currentIndex, setCurrentIndex] = useState(0)
     const [alertLukket, setAlertLukket] = useState(false)
     const [json, setJson] = useState(false)
+    const [mobil, setMobil] = useState(false)
     const handlePrevious = useCallback(() => {
         setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0))
     }, [setCurrentIndex])
@@ -63,7 +64,7 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
             {json && <pre>{JSON.stringify(eventGruppe.events, null, 2)}</pre>}
             {!json && (
                 <div className="">
-                    <Screenshot eventgruppe={eventGruppe}></Screenshot>
+                    <Screenshot eventgruppe={eventGruppe} mobil={mobil}></Screenshot>
                 </div>
             )}
             <div className="space-x-4 fixed items-center inset-x-0 bottom-0 bg-gray-200 p-4 flex">
@@ -80,8 +81,16 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
                 >
                     Neste
                 </Button>
-                <Switch>Mobil</Switch>
                 <Switch
+                    checked={mobil}
+                    onClick={() => {
+                        setMobil(!mobil)
+                    }}
+                >
+                    Mobil
+                </Switch>
+                <Switch
+                    checked={json}
                     onClick={() => {
                         setJson(!json)
                     }}
@@ -93,24 +102,50 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
     )
 }
 
-const Screenshot = ({ eventgruppe }: { eventgruppe: EventGruppe }) => {
+const Screenshot = ({ eventgruppe, mobil }: { eventgruppe: EventGruppe; mobil: boolean }) => {
     function imagePng() {
-        if (eventgruppe.url === 'www.nav.no/') {
+        const urlUtenTrailingSlash = eventgruppe.url.replace(/\/$/, '')
+
+        if (urlUtenTrailingSlash === 'www.nav.no') {
             return 'www.nav.no'
         }
-        if (eventgruppe.url === 'www.nav.no/kontaktoss') {
+        if (urlUtenTrailingSlash === 'www.nav.no/kontaktoss') {
             return 'www.nav.no.kontaktoss'
         }
-        if (eventgruppe.url === 'www.nav.no/syk/sykepengesoknad') {
+        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad') {
             return 'www.nav.no.syk.sykepengesoknad'
         }
+        if (urlUtenTrailingSlash === 'www.nav.no/minside') {
+            return 'www.nav.no.minside'
+        }
+        if (urlUtenTrailingSlash === 'www.nav.no/minside/nb/varsler') {
+            return 'www.nav.no.minside.nb.varsler'
+        }
+        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]/1') {
+            return 'soknad-forsteside'
+        }
+        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]') {
+            return 'soknad-forsteside'
+        }
+        if (urlUtenTrailingSlash.startsWith('www.nav.no/syk/sykepengesoknad/soknader/[redacted]/')) {
+            return 'soknad-sporsmal'
+        }
+        return null
     }
 
-    const png = imagePng()
-    if (png) {
-        // eslint-disable-next-line @next/next/no-img-element
-        return <img style={{ maxHeight: '70vh' }} src={'/static/amplitude/' + png + '.png'} alt="screenshot" />
+    const filnavn = imagePng()
+    if (filnavn) {
+        const plattform = mobil ? 'mobil' : 'desktop'
+        if (filnavn) {
+            return (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                    style={{ maxHeight: '70vh' }}
+                    src={'/static/amplitude/' + plattform + '/' + filnavn + '.png'}
+                    alt="screenshot"
+                />
+            )
+        }
     }
-
     return null
 }
