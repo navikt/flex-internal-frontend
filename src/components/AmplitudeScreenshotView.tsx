ac07ee4673db8b5713a0cd7b75e7ce4fa351b618
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-25 09:20:23 +0200

    Iframer spørsmål (#454)

--- src/components/AmplitudeScreenshotView.tsx
+++ src/components/AmplitudeScreenshotView.tsx
@@ -1,14 +1,25 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Alert, BodyShort, Button, Link, Switch } from '@navikt/ds-react'
+import dayjs from 'dayjs'
 
 import { AmplitudeResponse } from '../queryhooks/useAmplitudedata'
 
-import { EventGruppe, parseEvents } from './eventParsing'
+import { EventGruppeMedDetaljer, parseEvents } from './eventParsing'
 
 export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) => {
     const parsedEvents = parseEvents(data.events)
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [alertLukket, setAlertLukket] = useState(false)
+    const [alertLukket, setAlertLukket] = useState(() => {
+        const lukketDato = localStorage.getItem('alertLukketDato')
+        if (lukketDato) {
+            const lagretDato = dayjs(lukketDato)
+            const forskjellIDager = dayjs().diff(lagretDato, 'day')
+
+            // Sjekk om det har gått mer enn 14 dager
+            return forskjellIDager < 14
+        }
+        return false
+    })
     const [json, setJson] = useState(false)
     const [mobil, setMobil] = useState(false)
     const handlePrevious = useCallback(() => {
@@ -42,7 +53,10 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
                     size="small"
                     className="mb-4"
                     closeButton={true}
-                    onClose={() => setAlertLukket(true)}
+                    onClose={() => {
+                        setAlertLukket(true)
+                        localStorage.setItem('alertLukketDato', dayjs().toString()) // Lagrer tiden i millisekunder siden epoch
+                    }}
                 >
                     Screenshotsene er ikke en til en med eventene i de fleste tilfeller. Screenshots kan også bli
                     utdatert.
@@ -60,6 +74,9 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
                         </BodyShort>
                     )
                 })}
+                <BodyShort>{eventGruppe.sidevisning}</BodyShort>
+                <BodyShort>{eventGruppe.spormalTag}</BodyShort>
+                <BodyShort>{eventGruppe.soknadstype}</BodyShort>
             </div>
             {json && <pre>{JSON.stringify(eventGruppe.events, null, 2)}</pre>}
             {!json && (
@@ -102,53 +119,104 @@ export const AmplitudeScreenshotView = ({ data }: { data: AmplitudeResponse }) =
     )
 }
 
-const Screenshot = ({ eventgruppe, mobil }: { eventgruppe: EventGruppe; mobil: boolean }) => {
+const Screenshot = ({ eventgruppe, mobil }: { eventgruppe: EventGruppeMedDetaljer; mobil: boolean }) => {
     const urlUtenTrailingSlash = eventgruppe.url.replace(/\/$/, '')
     const plattform = mobil ? 'mobil' : 'desktop'
 
     function imagePng() {
-        if (urlUtenTrailingSlash === 'www.nav.no') {
-            return 'www.nav.no'
+        switch (urlUtenTrailingSlash) {
+            case 'www.nav.no':
+                return 'www.nav.no'
+
+            case 'www.nav.no/kontaktoss':
+                return 'www.nav.no.kontaktoss'
+
+            case 'www.nav.no/minside':
+                return 'www.nav.no.minside'
+
+            case 'www.nav.no/min-side':
+                return 'www.nav.no.min-side'
+
+            case 'www.nav.no/minside/nb/varsler':
+                return 'www.nav.no.minside.nb.varsler'
+
+            case 'www.nav.no/utlogget':
+                return 'www.nav.no.utlogget'
+
+            case 'www.nav.no/skriv-til-oss':
+                return 'www.nav.no.skriv-til-oss'
+
+            case 'person.nav.no/dokumentarkiv/tema/SYK':
+                return 'person.nav.no.dokumentarkiv.tema.syk'
+
+            case 'person.nav.no/dokumentarkiv/tema/SYM':
+                return 'person.nav.no.dokumentarkiv.tema.sym'
+
+            case 'person.nav.no/dokumentarkiv':
+            case 'www.nav.no/dokumentarkiv':
+                return 'person.nav.no.dokumentarkiv'
+
+            default:
+                if (urlUtenTrailingSlash.startsWith('www.nav.no/syk/sykepengesoknad/soknader/[redacted]/')) {
+                    return 'soknad-sporsmal'
+                }
+                return null
         }
-        if (urlUtenTrailingSlash === 'www.nav.no/kontaktoss') {
-            return 'www.nav.no.kontaktoss'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/minside') {
-            return 'www.nav.no.minside'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/min-side') {
-            return 'www.nav.no.min-side'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/minside/nb/varsler') {
-            return 'www.nav.no.minside.nb.varsler'
-        }
-        if (
-            urlUtenTrailingSlash === 'person.nav.no/dokumentarkiv' ||
-            urlUtenTrailingSlash === 'www.nav.no/dokumentarkiv'
-        ) {
-            return 'person.nav.no.dokumentarkiv'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/utlogget') {
-            return 'www.nav.no.utlogget'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/skriv-til-oss') {
-            return 'www.nav.no.skriv-til-oss'
-        }
-        if (urlUtenTrailingSlash === 'person.nav.no/dokumentarkiv/tema/SYK') {
-            return 'person.nav.no.dokumentarkiv.tema.syk'
-        }
-        if (urlUtenTrailingSlash === 'person.nav.no/dokumentarkiv/tema/SYM') {
-            return 'person.nav.no.dokumentarkiv.tema.sym'
-        }
-        if (
-            urlUtenTrailingSlash.startsWith('www.nav.no/syk/sykepengesoknad/soknader/[redacted]/') &&
-            urlUtenTrailingSlash !== 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]/1'
-        ) {
-            return 'soknad-sporsmal'
-        }
-        return null
     }
 
+    function iframedUrl() {
+        const demoSpmSide = eventgruppeTilDemoSporsmalSide(eventgruppe)
+        if (demoSpmSide) {
+            return demoSpmSide
+        }
+
+        switch (urlUtenTrailingSlash) {
+            case 'www.nav.no/syk/sykmeldinger/[redacted]':
+                return 'https://sykmeldinger.ekstern.dev.nav.no/syk/sykmeldinger/b4f4172f-4bb8-4512-b572-03f211b26f08'
+
+            case 'www.nav.no/syk/sykefravaer':
+                return 'https://demo.ekstern.dev.nav.no/syk/sykefravaer?testperson=syk-17-siden'
+
+            case 'www.nav.no/syk/sykmeldinger':
+                return 'https://sykmeldinger.ekstern.dev.nav.no/syk/sykmeldinger'
+
+            case 'www.nav.no/syk/oppfolgingsplaner/sykmeldt':
+                return 'https://demo.ekstern.dev.nav.no/syk/oppfolgingsplaner/sykmeldt'
+
+            case 'www.nav.no/syk/sykepengesoknad':
+                return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad'
+
+            case 'www.nav.no/syk/sykepenger':
+                if (eventgruppe.events.some((e) => e.event_type == 'skjema åpnet')) {
+                    return 'https://demo.ekstern.dev.nav.no/syk/sykepenger?id=a147e9a9-0aa2-4f5f-a8e3-c16c901e4071'
+                }
+                return 'https://demo.ekstern.dev.nav.no/syk/sykepenger'
+
+            case 'www.nav.no/syk/sykepengesoknad/avbrutt/[redacted]':
+                return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/avbrutt/811d15b2-2a76-4623-9530-1ba55617e0a5?testperson=integrasjon-soknader'
+
+            case 'www.nav.no/syk/meroppfolging/snart-slutt-pa-sykepengene':
+                return 'https://demo.ekstern.dev.nav.no/syk/meroppfolging/snart-slutt-pa-sykepengene'
+
+            case 'www.nav.no/syk/sykepengesoknad/kvittering/[redacted]':
+                return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/kvittering/3848e75e-4069-4076-95c0-3f9f0b63e498?testperson=integrasjon-soknader'
+
+            case 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]/1':
+            case 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]':
+                return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/soknader/faba11f5-c4f2-4647-8c8a-58b28ce2f3ef/1'
+
+            default:
+                return null
+        }
+    }
+
+    const iframed = iframedUrl()
+    if (iframed) {
+        if (mobil) {
+            return <iframe src={iframed} style={{ width: '390px', height: '844px' }} title="Iframed"></iframe>
+        }
+        return <iframe src={iframed} style={{ width: '100%', height: '70vh' }} title="Iframed"></iframe>
+    }
     const filnavn = imagePng()
     if (filnavn) {
         return (
@@ -160,53 +228,45 @@ const Screenshot = ({ eventgruppe, mobil }: { eventgruppe: EventGruppe; mobil: b
             />
         )
     }
+    return null
+}
 
-    function iframedUrl() {
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykmeldinger/[redacted]') {
-            return 'https://sykmeldinger.ekstern.dev.nav.no/syk/sykmeldinger/b4f4172f-4bb8-4512-b572-03f211b26f08'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykefravaer') {
-            return 'https://demo.ekstern.dev.nav.no/syk/sykefravaer?testperson=syk-17-siden'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykmeldinger') {
-            return 'https://sykmeldinger.ekstern.dev.nav.no/syk/sykmeldinger'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/oppfolgingsplaner/sykmeldt') {
-            return 'https://demo.ekstern.dev.nav.no/syk/oppfolgingsplaner/sykmeldt'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad') {
-            return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepenger') {
-            if (eventgruppe.events.some((e) => e.event_type == 'skjema åpnet')) {
-                return 'https://demo.ekstern.dev.nav.no/syk/sykepenger?id=a147e9a9-0aa2-4f5f-a8e3-c16c901e4071'
-            }
-            return 'https://demo.ekstern.dev.nav.no/syk/sykepenger'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/avbrutt/[redacted]') {
-            return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/avbrutt/811d15b2-2a76-4623-9530-1ba55617e0a5?testperson=integrasjon-soknader'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/meroppfolging/snart-slutt-pa-sykepengene') {
-            return 'https://demo.ekstern.dev.nav.no/syk/meroppfolging/snart-slutt-pa-sykepengene'
-        }
-        if (urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/kvittering/[redacted]') {
-            return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/kvittering/3848e75e-4069-4076-95c0-3f9f0b63e498?testperson=integrasjon-soknader'
-        }
-        if (
-            urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]/1' ||
-            urlUtenTrailingSlash === 'www.nav.no/syk/sykepengesoknad/soknader/[redacted]'
-        ) {
-            return 'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/soknader/faba11f5-c4f2-4647-8c8a-58b28ce2f3ef/1'
-        }
+function eventgruppeTilDemoSporsmalSide(e: EventGruppeMedDetaljer): string | null {
+    if (e.sidevisning !== 'soknadssporsmal') {
         return null
     }
-
-    const iframed = iframedUrl()
-    if (iframed) {
-        if (mobil) {
-            return <iframe src={iframed} style={{ width: '390px', height: '844px' }} title="Iframed"></iframe>
-        }
-        return <iframe src={iframed} style={{ width: '100%', height: '70vh' }} title="Iframed"></iframe>
+    const soknadstype = e.soknadstype
+    const sporsmal = e.spormalTag
+    if (!soknadstype || !sporsmal) {
+        return null
     }
+    switch (soknadstype) {
+        case 'ARBEIDSTAKERE': {
+            const base =
+                'https://demo.ekstern.dev.nav.no/syk/sykepengesoknad/soknader/faba11f5-c4f2-4647-8c8a-58b28ce2f3ef/'
+            switch (sporsmal) {
+                case 'ANSVARSERKLARING':
+                    return base + '1'
+                case 'TILBAKE_I_ARBEID':
+                    return base + '2'
+                case 'FERIE_V2':
+                    return base + '3'
+                case 'PERMISJON_V2':
+                    return base + '4'
+                case 'JOBBET_DU_GRADERT':
+                    // TODO en annen base
+                    return base + '5'
+                case 'ARBEID_UNDERVEIS_100_PROSENT':
+                    return base + '5'
+                case 'ANDRE_INNTEKTSKILDER_V2':
+                    return base + '6'
+                case 'UTLAND_V2':
+                    return base + '7'
+                case 'TIL_SLUTT':
+                    return base + '8'
+            }
+        }
+    }
+
     return null
 }
