04cfc4654438c977388757141e1367042a7f97f6
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-04 11:49:03 +0200

    Flex istedenfor internal

--- src/testdata/testdata.ts
+++ src/testdata/testdata.ts
@@ -820,7 +820,7 @@ export async function mockApi(opts: BackendProxyOpts): Promise<void> {
         res.end()
         return
     }
-    if (validert.api === 'POST /api/v1/internal/arbeidssokerperioder') {
+    if (validert.api === 'POST /api/v1/flex/arbeidssokerperioder') {
         res.status(200).json({
             arbeidssokerperioder: [
                 {
