4666d424641e24804bb25c091fee715d8c7b1ae1
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-10-14 16:27:00 +0200

    Kall hentVentetid med knapp

--- src/pages/ventetid.tsx
+++ src/pages/ventetid.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Label, TextField } from '@navikt/ds-react'
+import { Button, Label, TextField } from '@navikt/ds-react'
 import { allExpanded, defaultStyles, JsonView } from 'react-json-view-lite'
 
 import { initialProps } from '../initialprops/initialProps'
@@ -8,21 +8,31 @@ import { useVentetid } from '../queryhooks/useVentetid'
 const VentetidPage = () => {
     const [fnr, setFnr] = useState<string>()
     const [sykmeldingId, setSykmeldingId] = useState<string>()
+    const [doFetch, setDoFetch] = useState(false)
 
-    const { data } = useVentetid(fnr, sykmeldingId, !!fnr && !!sykmeldingId)
+    const { data } = useVentetid(fnr, sykmeldingId, doFetch && !!fnr && !!sykmeldingId)
 
     return (
         <div className="flex-row space-y-4">
             <TextField
-                label="fnr"
-                onChange={(e) => (e.target.value.length === 11 ? setFnr(e.target.value) : setFnr(undefined))}
+                label="Fnr"
+                onChange={(e) => {
+                    const value = e.target.value
+                    setDoFetch(false)
+                    value.length === 11 ? setFnr(value) : setFnr(undefined)
+                }}
             />
             <TextField
                 label="SykmeldingId"
-                onChange={(e) =>
-                    e.target.value.length > 0 ? setSykmeldingId(e.target.value) : setSykmeldingId(undefined)
-                }
+                onChange={(e) => {
+                    const value = e.target.value
+                    setDoFetch(false)
+                    value.length > 0 ? setSykmeldingId(value) : setSykmeldingId(undefined)
+                }}
             />
+            <Button onClick={() => setDoFetch(true)} disabled={!fnr || !sykmeldingId} variant="primary" size="small">
+                Hent ventetid
+            </Button>
             <div>
                 <Label>Ventetid</Label>
                 {data && <JsonView data={data} shouldExpandNode={allExpanded} style={defaultStyles} />}
