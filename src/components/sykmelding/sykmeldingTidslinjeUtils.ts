c49cd23ce246cf5bc66f89a2ad0fc26d31880a9e
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-07-01 10:48:25 +0200

    Legg til tid for signaturdato

--- src/components/sykmelding/sykmeldingTidslinjeUtils.ts
+++ src/components/sykmelding/sykmeldingTidslinjeUtils.ts
@@ -158,6 +158,16 @@ export const sorterPerioder = (perioder: PeriodeMedDatoer[]): PeriodeMedDatoer[]
 export const formaterDato = (dato: Date): string =>
     new Intl.DateTimeFormat('nb-NO', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(dato)
 
+export const formaterDatoMedTid = (dato: Date): string =>
+    new Intl.DateTimeFormat('nb-NO', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }).format(dato)
+
 export const antallKalenderdager = (fra: Date, til: Date): number => {
     const millisekunderPerDag = 24 * 60 * 60 * 1000
     return Math.floor((til.getTime() - fra.getTime()) / millisekunderPerDag) + 1
