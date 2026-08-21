975e674f303344d17df60e1eb189163e7e485979
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-08-21 10:14:08 +0200

    Bump next 16.3 fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     output: 'standalone',
+    outputFileTracingIncludes: {
+        '/*': [
+            './node_modules/@swc/helpers/**/*',
+            './node_modules/.pnpm/@swc+helpers@*/node_modules/@swc/helpers/**/*',
+        ],
+    },
 }
 
 export default nextConfig
