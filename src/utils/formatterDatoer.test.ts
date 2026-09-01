e1b79be564fcdb5175721258197d74e5c6a30256
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-09-01 14:00:20 +0200

    Gjør formatteringstest tidssoneuavhengig

--- src/utils/formatterDatoer.test.ts
+++ src/utils/formatterDatoer.test.ts
@@ -4,6 +4,6 @@ import { formatterTimestamp } from './formatterDatoer'
 
 describe('formatterTimestamp', () => {
     it('bruker norsk locale eksplisitt for månedsnavn', () => {
-        expect(formatterTimestamp('2026-05-15T12:34:00Z')).toBe('15 mai 2026 14:34')
+        expect(formatterTimestamp('2026-05-15T12:34:00Z')).toMatch(/^15 mai 2026 \d{2}:\d{2}$/)
     })
 })
